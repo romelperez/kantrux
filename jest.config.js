c8bfb0cfffc2e457fec19e3a70724f79d9ad8ade
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-01 18:02:28 +0800

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-
-module.exports = {
-  setupFilesAfterEnv: ['jest-extended'],
-  rootDir: 'src',
-  coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/node_modules/', '/lib/', '/dist/']
-};
